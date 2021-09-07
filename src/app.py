367cab916352d17381863e948560cc66ddd10d35
Emmanuel Oh <emmaneugene@outlook.com>
2021-09-07 08:39:37 +0800

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,204 @@
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('db_conn') + '/game' # 'mysql+mysqlconnector://cs302:cs302@localhost:3306/game'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
+                                           'pool_recycle': 280}
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class Game(db.Model):
+    __tablename__ = 'game'
+
+    game_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    platform = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    stock = db.Column(db.Integer)
+
+    def __init__(self, title, platform, price, stock):
+        self.title = title
+        self.platform = platform
+        self.price = price
+        self.stock = stock
+
+    def to_dict(self):
+        return {
+            'game_id': self.game_id,
+            'title': self.title,
+            'platform': self.platform,
+            'price': self.price,
+            'stock': self.stock
+        }
+
+@app.route('/health')
+def health_check():
+    return jsonify(
+            {
+                'message': 'Service is healthy.'
+            }
+    ), 200
+
+@app.route('/games')
+def get_all():
+    game_list = Game.query.all()
+    if len(game_list) != 0:
+        return jsonify(
+            {
+                'data': {
+                    'games': [game.to_dict() for game in game_list]
+                }
+            }
+        ), 200
+    return jsonify(
+        {
+            'message': 'There are no games.'
+        }
+    ), 404
+
+@app.route('/games/<int:game_id>')
+def find_by_id(game_id):
+    game = Game.query.filter_by(game_id=game_id).first()
+    if game:
+        return jsonify(
+            {
+                'data': game.to_dict()
+            }
+        ), 200
+    return jsonify(
+        {
+            'message': 'Game not found.'
+        }
+    ), 404
+
+@app.route('/games', methods=['POST'])
+def new_game():
+    try:
+        data = request.get_json()
+        game = Game(**data)
+        db.session.add(game)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(
+            {
+                'message': 'An error occurred creating the game.',
+                'error': str(e)
+            }
+        ), 500
+
+    return jsonify(
+        {
+            'data': game.to_dict()
+        }
+    ), 201
+
+@app.route('/games/<int:game_id>', methods=['PUT'])
+def update_game(game_id):
+    game = Game.query.filter_by(game_id=game_id).first()
+
+    if not game:
+        return jsonify(
+            { 
+                'data': {
+                    'game_id': game_id
+                },
+                'message': 'Game not found' 
+            }
+        ), 404
+
+    try:
+        data = request.get_json()
+        game.title = data['title']
+        game.platform = data['platform']
+        game.price = data['price']
+        game.stock = data['stock']
+        db.session.add(game)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(
+            {
+                'message': 'An error occurred replacing the game.',
+                'error': str(e)
+            }
+        ), 500
+
+    return jsonify(
+        {
+            'data': game.to_dict()
+        }
+    ), 200
+
+@app.route('/games/<int:game_id>', methods=['DELETE'])
+def delete_game(game_id):
+    game = Game.query.filter_by(game_id=game_id).first()
+
+    if not game:
+        return jsonify(
+            { 
+                'data': {
+                    'game_id': game_id
+                },
+                'message': 'Game not found' 
+            }
+        ), 404
+
+    try:
+        db.session.delete(game)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(
+            {
+                'message': 'An error occurred deleting the game.',
+                'error': str(e)
+            }
+        ), 500
+
+    return jsonify(
+        {
+            'data': { 'game_id': game_id }
+        }
+    ), 200
+
+@app.route('/games/<int:game_id>', methods=['PATCH'])
+def reserve_game_qty(game_id):
+    game = Game.query.filter_by(game_id=game_id).first()
+
+    if not game:
+        return jsonify(
+            { 
+                'data': {
+                    'game_id': game_id
+                },
+                'message': 'Game not found' 
+            }
+        ), 404
+
+    
+    data = request.get_json()
+    if game.stock >= data['reserve']:
+        game.stock -= data['reserve']
+        db.session.add(game)
+        db.session.commit()
+    else:
+        return jsonify(
+            {
+                'message': 'An error occurred updating the game.',
+                'error': 'Not enough games in stock.'
+            }
+        ), 500
+
+    return jsonify(
+        {
+            'data': game.to_dict()
+        }
+    ), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
